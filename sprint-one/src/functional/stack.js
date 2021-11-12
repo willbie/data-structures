c9f43ff1d319fca411aec592bd92d1e8c45b2791
Houheng Li <lihouheng0710@gmail.com>
2021-11-11 21:07:32 -0800

    complete

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,29 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var num = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[num] = value;
+    num = num + 1
   };
 
   someInstance.pop = function() {
+    var popped = storage[num - 1]
+    delete storage[num - 1];
+    num = num - 1;
+    return popped
   };
 
   someInstance.size = function() {
+    var length = 0
+    for (var key in storage) {
+      length = length + 1;
+    }
+    return length;
   };
 
   return someInstance;
 };
+
