92febb42c59e78e3bba3d3624d761845505a76c7
Houheng Li <lihouheng0710@gmail.com>
2021-11-11 21:07:13 -0800

    complete

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,29 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var num = 0;
+  var pop = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[num] = value;
+    num++;
   };
 
   someInstance.dequeue = function() {
+    var popped = storage[pop];
+    delete storage[pop];
+    pop++;
+    return popped;
   };
 
   someInstance.size = function() {
+    var length = 0
+    for (var key in storage) {
+      length = length + 1;
+    }
+    return length;
   };
 
   return someInstance;
