45858dbacbe86f8390cb386dda3bf29c5b55f406
Houheng Li <lihouheng0710@gmail.com>
2021-11-11 21:09:01 -0800

    complete

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,28 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.num = 0;
 };
 
+Stack.prototype = {
+  push : function(value) {
+    this.storage[this.num] = value;
+    this.num++;
+  },
 
+  pop : function() {
+    var popped = this.storage[this.num - 1]
+    delete this.storage[this.num - 1];
+    this.num--;
+    return popped;
+  },
+
+  size : function() {
+    var length = 0;
+    for (var key in this.storage) {
+      length = length + 1;
+    }
+    return length;
+  }
+}
